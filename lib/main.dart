9f49b598d662125b5fa9a68bba1da49b648e58ec
muhhammdsallam <m.sallam2622000@gmail.com>
2021-07-25 19:58:19 +0200

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,112 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget { 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [Colors.black, Colors.indigo.shade900])),
+        child: Scaffold(
+            backgroundColor: Colors.transparent,
+            body: SafeArea(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundImage: AssetImage('images/sallam.jpg'),
+                  radius: 50.0,
+                ),
+                Text(
+                  'Mohamed Sallam',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 30.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'SOFTWARE ENGINEER',
+                  style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    color: Colors.grey,
+                    fontSize: 15.0,
+                    letterSpacing: 3.5,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.grey.shade800,
+                  ),
+                ),
+                Card(
+                    margin:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 100.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.phone,
+                        color: Colors.black,
+                        size: 20.0,
+                      ),
+                      title: Text(
+                        '+20 106 386 0720',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 15.0,
+                        ),
+                      ),
+                    )),
+                Card(
+                    margin:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 100.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.email,
+                        color: Colors.black,
+                        size: 20.0,
+                      ),
+                      title: Text(
+                        'm.sallam2622000@gmail.com',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 15.0,
+                        ),
+                      ),
+                    )),
+                Card(
+                    margin:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 100.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.school,
+                        color: Colors.black,
+                        size: 20.0,
+                      ),
+                      title: Text(
+                        'FOE Alexandria University',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 15.0,
+                        ),
+                      ),
+                    ))
+              ],
+            ))),
       ),
-    ),
-  );
+    );
+  }
 }
